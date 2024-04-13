3395190f0d3a3244a4a026bf1e8e08b06f7e5df3
MackanGut <166909074+MackanGut@users.noreply.github.com>
2024-04-13 12:21:31 +0200

    Update pizza.jsx

--- pizza.jsx
+++ pizza.jsx
@@ -10,73 +10,73 @@ export default function Pizza() {
             <div className="grid">
                 <div className="item">
                   <div>
-                    <h2>Capricciosa</h2>
+                    <h2>1. Capricciosa</h2>
                     <h3>Skinka, champinjoner</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Milano</h2>
+                    <h2>2. Milano</h2>
                     <h3>Skinka</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Margareta</h2>
+                    <h2>3. Margareta</h2>
                     <h3>Tomat, ost</h3>
                   </div>
-                  <h4>115kr</h4>
+                  <h4>115:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Capri</h2>
+                    <h2>4. Capri</h2>
                     <h3>Skinka, räkor</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Hawaii</h2>
+                    <h2>5. Hawaii</h2>
                     <h3>Skinka, ananas</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Calzone</h2>
+                    <h2>6. Calzone</h2>
                     <h3>(Inbakad) skinka</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Palermo</h2>
+                    <h2>7. Palermo</h2>
                     <h3>Köttfärs, lök</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Sicilia</h2>
+                    <h2>8. Sicilia</h2>
                     <h3>Champinjoner</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Roma</h2>
+                    <h2>9 Roma</h2>
                     <h3>Räkor, tonfisk</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Afrikane</h2>
+                    <h2>10. Afrikane</h2>
                     <h3>Ananas, banan</h3>
                   </div>
-                  <h4>120kr</h4>
+                  <h4>120:-</h4>
                 </div>
             </div>
             <div className="titel">
@@ -87,52 +87,52 @@ export default function Pizza() {
             <div className="grid">
                 <div className="item">
                   <div>
-                    <h2>Campino</h2>
+                    <h2>11. Campino</h2>
                     <h3>Skinka, räkor, ananas</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pirog</h2>
-                    <h3>(Inbakad) skinska, lök, köttfärs</h3>
+                    <h2>12. Pirog</h2>
+                    <h3>(Inbakad) skinka, lök, köttfärs</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Disco</h2>
-                    <h3>Skinska, räkor, köttfärs</h3>
+                    <h2>13. Disco</h2>
+                    <h3>Skinka, räkor, köttfärs</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Salamai</h2>
+                    <h2>14. Salamai</h2>
                     <h3>Skinka, salami</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Beiso</h2>
+                    <h2>15. Beiso</h2>
                     <h3>Skinka, rå lök, kebabsås</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Corallo</h2>
+                    <h2>16. Corallo</h2>
                     <h3>Bacon, lök, paprika</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Jamaica</h2>
+                    <h2>17. Jamaica</h2>
                     <h3>Skinka, räkor, champinjoner</h3>
                   </div>
-                  <h4>125kr</h4>
+                  <h4>125:-</h4>
                 </div>
             </div>
             <div className="titel">
@@ -142,66 +142,66 @@ export default function Pizza() {
             <div className="grid">
                 <div className="item">
                   <div>
-                    <h2>Vegetarisk</h2>
+                    <h2>18. Vegetarisk</h2>
                     <h3>Champinjoner, paprika, lök, ananas, kronärtskocka</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Istanbul</h2>
-                    <h3>Skinka, champinjoner, fläskfilé</h3>
-                  </div>
-                  <h4>130kr</h4>
-                </div>
-                <div className="item">
-                  <div>
-                    <h2>Rima Special</h2>
+                    <h2>19. Istanbul</h2>
                     <h3>Skinka, champinjoner, paprika, lök, fläskfilé</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Damaskus Special</h2>
-                    <h3>Skinka, champinjoner, fläskfilé, lök, bearnaisesås</h3>
+                    <h2>20. Rima Special</h2>
+                    <h3>Skinka, champinjoner, paprika, lök, fläskfilé</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>La Mare</h2>
+                    <h2>21. Damaskus Special</h2>
+                    <h3>Skinka, fläskfilé, färska tomater, bearnaisesås</h3>
+                  </div>
+                  <h4>130:-</h4>
+                </div>
+                <div className="item">
+                  <div>
+                    <h2>22. La Mare</h2>
                     <h3>Skinka, räkor, champinjoner, ananas</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Rikard</h2>
+                    <h2>23. Rikard</h2>
                     <h3>Räkor, fläskfilé, bearnaisesås</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Emelia</h2>
+                    <h2>24. Emelia</h2>
                     <h3>Skinka, räkor, champinjoner, tonfisk</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Ann Louise</h2>
+                    <h2>25. Ann Louise</h2>
                     <h3>Färska tomater, fläskfilé, bearnaisesås</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Quattro Stagione</h2>
+                    <h2>26. Quattro Stagione</h2>
                     <h3>Skinka, champinjoner, musslor, räkor, kronärtskocka</h3>
                   </div>
-                  <h4>130kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 
                 
@@ -214,59 +214,59 @@ export default function Pizza() {
             <div className="grid">
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>27. Ciao Ciao</h2>
+                    <h3>(Inbakad) Champinjoner, lök, vitlök, fläskfilé</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>28. Dubbel el Ciao Ciao</h2>
+                    <h3>Flygande tefat</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>150:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>29. Linda</h2>
+                    <h3>Skinka, champinjoner, räkor, kronärtskocka</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>30. Mikael</h2>
+                    <h3>Skinka, fläskfilé, banan, curry</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>31. Elisabeth</h2>
+                    <h3>Paprika, lök, fläskfilé, varma färska tomater</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>32. Havspizza</h2>
+                    <h3>Räkor, musslor, crabfish & tonfisk</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>33. Flygande Jakob</h2>
+                    <h3>Kyckling, banan, bacon, curry</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>34. Kycklingpizza</h2>
+                    <h3>Kyckling, bearnaisesås</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 
                 
@@ -278,60 +278,82 @@ export default function Pizza() {
             <div className="grid">
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>36. Tacopizza</h2>
+                    <h3>Lök, köttfärs blandfärs, jalapeno, tacokrydda, chilisås</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>37. Mexicana</h2>
+                    <h3>Skinka, blandförs, jalapeno, tacokrydda, chilisås</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>130:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>38. Black & White</h2>
+                    <h3>Fläskfilé, oxfilé, kebabsås, bearnisesås</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>145:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>39. Boxholm special</h2>
+                    <h3>Champinjoner, lök, oxfilé, gorgonzola</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>145:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>40. Superkebabpizza</h2>
+                    <h3>Skinka, kebabkött, kebabsås, pommes, sallad, gurka, lök, tomat</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>160:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>41. Bebbos Special</h2>
+                    <h3>Grön paprika, röd paprika, purjolök, svarta oliver, mozzarella, färska champinjoner & färsk vitlök</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>140:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>42. Italia</h2>
+                    <h3>Salami, lök, soltorkade tomater, mozzarella, ruccolasallad, färsk vitlök</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>150:-</h4>
                 </div>
                 <div className="item">
                   <div>
-                    <h2>Pizza</h2>
-                    <h3>Sås och sånt</h3>
+                    <h2>43. Parma</h2>
+                    <h3>Färska champinjoner, mozzarella, lufttorkad skinka, ruccolasallad, färsk vitlök</h3>
                   </div>
-                  <h4>90kr</h4>
+                  <h4>150:-</h4>
                 </div>
+              <div className="item">
+                  <div>
+                    <h2>44. Poker</h2>
+                    <h3>Färska champinjoner, salami, bacon, lök, mozzarella, färsk vitlök</h3>
+                  </div>
+                  <h4>150:-</h4>
+                </div>
+              <div className="item">
+                  <div>
+                    <h2>45. Pippi</h2>
+                    <h3>Färska champinjoner, soltorkade tomater, oxfilé, gorgonzolla, mozzarella</h3>
+                  </div>
+                  <h4>155:-</h4>
+                </div>
+              <div className="item">
+                  <div>
+                    <h2>46. Parmesan</h2>
+                    <h3>Lufttorkad skinka, soltorkade tomater, parmesan, mozzarella & ruccola sallad</h3>
+                  </div>
+                  <h4>150:-</h4>
+                </div>
+              
                 
                 
             </div>
